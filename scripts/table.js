cd5863f76f5d02d99256e8473d33b0cf04ee8769
Fishbite <43952420+Fishbite@users.noreply.github.com>
2024-05-01 04:57:22 +0100

    tables

--- scripts/table.js
+++ scripts/table.js
@@ -1,1 +1,35 @@
 console.log("Table.js");
+
+// const table = document.getElementById("age-table");
+// console.log(table);
+
+// let labels = document.querySelectorAll("#age-table label");
+// labels = table.getElementsByTagName("label");
+// labels = table.querySelectorAll("label");
+// console.log(labels);
+
+// console.log(table.rows[0].firstElementChild);
+// console.log(table.rows[0].cells[0]);
+// console.log(table.getElementsByTagName("td")[0]);
+
+// console.log(document.getElementsByName("search")[0]);
+// console.log(document.querySelector("form[name='search']"));
+
+// console.log(table.querySelector("input"));
+// console.log(table.getElementsByTagName("input")[0]);
+// console.log(table.getElementsByTagName("input")[5]);
+
+// let input = table.getElementsByTagName("input");
+// console.log(input[input.length - 1]);
+// console.log(
+//   table.lastElementChild.firstElementChild.nextElementSibling.lastElementChild
+//     .children[2]
+// );
+// console.dir(table);
+
+// console.log(document.body.lastChild.nodeType);
+// console.log(table.rows[1].cells[1].firstElementChild.attributes);
+
+// let att = document.querySelector("div");
+// console.log(att.getAttribute("data-widget-name"));
+// console.log(att.dataset.widgetName);
