313a2ac7294109a1127fa989c7ad0d47c0881166
Fishbite <43952420+Fishbite@users.noreply.github.com>
2024-07-19 14:18:41 +0100

    added drawing functionality

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -29,9 +29,9 @@ line("hsl(46, 45%, 45%)", 20, 119, 128, 119, 256);
 line("hsl(46, 45%, 45%)", 10, 148, 184, 212, 184);
 line("hsl(46, 45%, 45%)", 10, 207, 184, 207, 96);
 
-ctx.fillStyle = "hsla(46, 45%, 45%, 0.5)";
-ctx.strokeRect(36, 64, 64, 50);
-ctx.fillRect(36, 64, 64, 50);
+// ctx.fillStyle = "hsla(46, 45%, 45%, 0.5)";
+// ctx.strokeRect(36, 64, 64, 50);
+// ctx.fillRect(36, 64, 64, 50);
 
 let ww = window.innerWidth;
 let wh = window.innerHeight;
@@ -40,4 +40,45 @@ let datumX = ww / 2 - canvas.width / 2;
 let datumY = wh / 2 - canvas.height / 2;
 console.log(ww, datumX);
 
-onmousemove = (e) => console.log("mouse location:", e.x - datumX, e.y - datumY);
+// onmousemove = (e) => console.log("mouse location:", e.x - datumX, e.y - datumY);
+
+canvas.addEventListener("click", drawLine, false);
+document.addEventListener("keydown", handleKey, false);
+
+let drawing = false;
+let tx, ty;
+
+function drawLine(e) {
+  if (!drawing) {
+    tx = e.clientX - datumX;
+    ty = e.clientY - datumY;
+    ctx.beginPath();
+    ctx.moveTo(tx, ty);
+    drawing = true;
+  } else {
+    console.log("drawing = ", drawing);
+    tx = e.clientX - datumX;
+    ty = e.clientY - datumY;
+    ctx.lineTo(tx, ty);
+    ctx.stroke();
+  }
+  console.log(tx, ty);
+}
+
+function handleKey(e) {
+  console.log(e.key);
+
+  if (isFinite(e.key)) {
+    ctx.lineWidth = e.key;
+  }
+
+  if (e.key === "Enter") {
+    drawing = false;
+  }
+
+  if (e.key === "c") {
+    ctx.closePath();
+    ctx.stroke();
+    drawing = false;
+  }
+}
