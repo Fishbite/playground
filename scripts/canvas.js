6c5662f1aa6c55d607fbe4450fc07492bca18f05
Fishbite <43952420+Fishbite@users.noreply.github.com>
2024-07-19 04:51:15 +0100

    canvas setup

---
+++ scripts/canvas.js
@@ -0,0 +1,9 @@
+console.log("canvas....");
+
+let canvas = document.createElement("canvas");
+canvas.style.border = "1px solid black";
+canvas.style.backgroundColor = "rgb(50 25 10)";
+canvas.style.width = "256px";
+canvas.style.height = "256px";
+document.body.appendChild(canvas);
+let ctx = canvas.getContext("2d");
